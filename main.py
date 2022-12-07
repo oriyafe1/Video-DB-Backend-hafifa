fba2dc65280ea92d5b8b5847d57b95d199d7ea46
oriyafe1 <ori987yafe@gmail.com>
2022-12-07 17:48:59 +0200

    Saving frames into s3

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import sys
+
 from given_functions import is_frame_tagged, generate_metadata
 from models import Video, Metadata, Frame, session
 from flask import Flask, request
 from minio import Minio
+from io import BytesIO
 import cv2
 import os
 
@@ -11,15 +14,16 @@ minio_client = Minio('localhost:9000', access_key='s3manager', secret_key='s3man
 minio_bucket_name = 'bionic'
 
 
-def get_frame_metadata(frame):
+def save_frame_metadata(frame):
     is_threat = is_frame_tagged(frame)
     fov, azimuth, elevation = generate_metadata(frame)
+    frame_metadata = Metadata(is_threat=is_threat, fov=fov, azimuth=azimuth, elevation=elevation)
+    session.add(frame_metadata)
 
-    return Metadata(is_threat=is_threat, fov=fov, azimuth=azimuth, elevation=elevation)
+    return frame_metadata
 
 
-def save_video(video_path):
-    video_filename = os.path.basename(video_path)
+def save_video(video_path, video_filename):
     observation_post_name = video_filename.split("_")[0]
     video_os_filepath = f'/videos/{video_filename}'
     minio_client.fput_object(minio_bucket_name, video_os_filepath, video_path)
@@ -29,12 +33,25 @@ def save_video(video_path):
     return video_instance
 
 
+def save_frame(frame, curr_frame_index, video_instance, video_name, frame_metadata):
+    frame_os_filepath = f'/frames/{video_name}/frame_{curr_frame_index}.jpg'
+    _, jpeg_frame = cv2.imencode('.jpg', frame)
+    image_bytes = BytesIO(jpeg_frame)
+    minio_client.put_object(minio_bucket_name, frame_os_filepath,
+                            image_bytes,
+                            len(jpeg_frame))
+
+    frame_db_instance = Frame(video_id=video_instance.id, metadata_id=frame_metadata.id,
+                              OS_filepath=frame_os_filepath,
+                              frame_index=curr_frame_index)
+    session.add(frame_db_instance)
+
+
 @app.post("/video")
 def upload_video_from_local_path():
     video_path = request.json['path']
-
-    video_instance = save_video(video_path)
-
+    video_name = os.path.basename(video_path)
+    video_instance = save_video(video_path, video_name)
     video = cv2.VideoCapture(video_path)
     curr_frame_index = 0
 
@@ -42,12 +59,8 @@ def upload_video_from_local_path():
         ret, frame = video.read()
 
         if ret:
-            curr_frame_metadata = get_frame_metadata(frame)
-            session.add(curr_frame_metadata)
-            frame_db_instance = Frame(video_id=video_instance.id, metadata_id=curr_frame_metadata.id,
-                                      OS_filepath='test',
-                                      frame_index=curr_frame_index)
-            session.add(frame_db_instance)
+            frame_metadata = save_frame_metadata(frame)
+            save_frame(frame, curr_frame_index, video_name, video_instance, frame_metadata)
             curr_frame_index += 1
         else:
             break
